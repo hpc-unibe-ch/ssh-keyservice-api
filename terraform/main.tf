36a66edd668dcb2583628e1c81024d719487eba5
Mathis Tomio <mathis.tomio@unibe.ch>
2025-11-26 14:50:20 +0100

    configured postgres flexible server

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,15 +25,63 @@ resource "azurerm_virtual_network" "example" {
     name             = "subnet2"
     address_prefixes = ["10.0.1.0/24"]
   }
+}
 
-  subnet {
-    name             = "subnet2"
-    address_prefixes = ["10.0.2.0/24"]
+resource "azurerm_subnet" "postgres" {
+  name                 = "postgresql-flexible-server"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Storage"]
+  delegation {
+    name = "fs"
+    service_delegation {
+      name = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
   }
+}
 
-  tags = {
-    environment = "Production"
-  }
+resource "azurerm_private_dns_zone" "postgres" {
+  name                = "privatelink.postgres.database.azure.com"
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+# resource "azurerm_private_dns_zone" "2" {
+#   name                = "dns_zone2"
+#   resource_group_name = azurerm_resource_group.this.name
+# }
+
+resource "azurerm_private_dns_zone_virtual_network_link" "example" {
+  name                  = "exampleVnetZone.com"
+  private_dns_zone_name = azurerm_private_dns_zone.postgres.name
+  virtual_network_id    = azurerm_virtual_network.example.id
+  resource_group_name   = azurerm_resource_group.this.name
+  depends_on            = [azurerm_subnet.example]
+}
+
+resource "azurerm_postgresql_flexible_server" "example" {
+  name                          = "example-psqlflexibleserver"
+  resource_group_name           = azurerm_resource_group.this.name
+  location                      = azurerm_resource_group.this.location
+  version                       = "12"
+  delegated_subnet_id           = azurerm_subnet.postgres.id
+  private_dns_zone_id           = azurerm_private_dns_zone.postgres.id
+  public_network_access_enabled = false
+  administrator_login           = "psqladmin"
+  administrator_password        = "H@Sh1CoR3!"
+  zone                          = "1"
+
+
+  storage_mb                   = 32768
+  storage_tier                 = "P4"
+  geo_redundant_backup_enabled = true
+
+  sku_name   = "B_Standard_B1ms"
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.example]
+
 }
 
 resource "azurerm_app_service_plan" "sshkeyservice" {
@@ -47,14 +95,4 @@ resource "azurerm_app_service_plan" "sshkeyservice" {
     tier = "Standard"
     size = "B1"
   }
-}
-
-resource "azurerm_private_dns_zone" "1" {
-  name                = "privatelink.postgres.database.azure.com"
-  resource_group_name = azurerm_resource_group.this.name
-}
-
-resource "azurerm_private_dns_zone" "2" {
-  name                = "privatelink.redis.cache.windows.net"
-  resource_group_name = azurerm_resource_group.this.name
 }
