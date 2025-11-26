5649e943ac635297a8446bee60dfcdc16078dd2d
Mathis Tomio <mathis.tomio@unibe.ch>
2025-11-26 13:12:25 +0100

    fixed resource group naming

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,14 +5,14 @@ resource "azurerm_resource_group" "this" {
 
 resource "azurerm_network_security_group" "example" {
   name                = "example-security-group"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
 }
 
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
