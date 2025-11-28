280423563038febee87c5e4925069e22a07cc3f9
Mathis Tomio <mathis.tomio@unibe.ch>
2025-11-28 13:43:53 +0100

    configured web-app

--- terraform/app.tf
+++ terraform/app.tf
@@ -47,17 +47,46 @@ resource "azurerm_linux_web_app" "api" {
   }
 
   connection_string {
-    name  = "test"
+    name  = "pgdb"
     type  = "PostgreSQL"
     value = azurerm_postgresql_flexible_server.example.id
   }
 }
 
-# resource "azurerm_linux_web_app" "web" {
-#   name                = "ssh-keyservice-web-prod"
-#   resource_group_name = azurerm_resource_group.this.name
-#   location            = azurerm_resource_group.this.location
-#   service_plan_id     = azurerm_service_plan.sshkeyservice.id
+resource "azurerm_linux_web_app" "web" {
+  # checkov:skip=CKV_AZURE_17: "Ensure the web app has 'Client Certificates (Incoming client certificates)' set"
+  # checkov:skip=CKV_AZURE_16: "Ensure that Register with Azure Active Directory is enabled on App Service"
+  # checkov:skip=CKV_AZURE_66: "Ensure that App service enables failed request tracing"
+  # checkov:skip=CKV_AZURE_63: "Ensure that App service enables HTTP logging"
+  # checkov:skip=CKV_AZURE_88: "Ensure that app services use Azure Files"
+  # checkov:skip=CKV_AZURE_78: "Ensure FTP deployments are disabled"
+  # checkov:skip=CKV_AZURE_213: "Ensure that App Service configures health check"
+  name                      = "ssh-keyservice-web-prod"
+  resource_group_name       = azurerm_resource_group.this.name
+  location                  = azurerm_resource_group.this.location
+  service_plan_id           = azurerm_service_plan.sshkeyservice.id
+  https_only                = true
+  virtual_network_subnet_id = azurerm_subnet.app.id
+  # key_vault_reference_identity_id = azurerm_key_vault.example.id
+  public_network_access_enabled = false
 
-#   site_config {}
-# }
+  identity {
+    type = "SystemAssigned"
+  }
+
+  auth_settings {
+    enabled = true
+  }
+
+  logs {
+    detailed_error_messages = true
+  }
+
+  site_config {
+    # health_check_path = "/healthcheck" # Change to real health check path
+    http2_enabled = true
+    application_stack {
+      python_version = 3.12
+    }
+  }
+}
