1ef96e52995d53815aea1f32a488aa919ccaacf1
Mathis Tomio <mathis.tomio@unibe.ch>
2026-01-27 08:40:17 +0100

    configured api base url as env var

--- terraform/app.tf
+++ terraform/app.tf
@@ -161,5 +161,6 @@ resource "azurerm_linux_web_app" "frontend" {
   app_settings = {
     AZURE_KEY_VAULT_URL = azurerm_key_vault.vault-01.vault_uri
     AZURE_CLIENT_ID     = azurerm_user_assigned_identity.frontend-app.client_id
+    AZURE_API_BASE_URL  = "https://${azurerm_linux_web_app.api.default_hostname}"
   }
 }
